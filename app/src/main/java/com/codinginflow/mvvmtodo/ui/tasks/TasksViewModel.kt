78d7eccf906715147df714dacefa8feec0485604
Jared <73672469+Village1680@users.noreply.github.com>
2023-04-09 16:48:25 +1200

    get data from database stream into viewmodel

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -2,12 +2,19 @@ package com.codinginflow.mvvmtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 
+/*
+    viewmodel should never reference a activity/fragment as the view model lives much longer (eg fragment destruction on orientation change)
+    this would create a memory leak when the fragment is destroyed.
+ */
 
 // inject DAO to ViewModel
 class TasksViewModel @ViewModelInject constructor(
     //define dependency
     private val taskDao: TaskDao
 ) : ViewModel() {
+     // asLiveData is the latest value of the flow stream
+    val tasks = taskDao.getTasks().asLiveData()
 }
