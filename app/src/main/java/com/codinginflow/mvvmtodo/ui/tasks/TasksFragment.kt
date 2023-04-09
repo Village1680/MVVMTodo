e22d67a19018f93bc890181b0c5b307a7d59d09e
Jared <73672469+Village1680@users.noreply.github.com>
2023-04-09 20:54:41 +1200

    created functions to define behaviour onSort/Hide Clicked, updated methods called by fragment in this case

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -8,11 +8,16 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
 import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 // annotation lets dagger know to inject here
 @AndroidEntryPoint
@@ -55,6 +60,13 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
             // filter list in real time when type in search field
             viewModel.searchQuery.value = it
         }
+
+        // coroutine scope that lives as long as the view of the fragment lives (if fragment view is destroyed, cancel coroutine)
+        // read single value from preferences when starting app, and then cancel coroutine
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     // define what to do when item is clicked
@@ -63,17 +75,17 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         return when(item.itemId) {
             //define depending on item
             R.id.action_sort_by_name -> {
-                viewModel.sortOrder.value = SortOrder.BY_NAME
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
             R.id.action_sort_by_date_created -> {
-                viewModel.sortOrder.value = SortOrder.BY_DATE
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                 true
             }
             R.id.action_hide_completed_tasks -> {
                 // toggle checkbox
                 item.isChecked = !item.isChecked
-                viewModel.hideCompleted.value = item.isChecked
+                viewModel.onHideCompletedClick(item.isChecked)
                 true
             }
             R.id.action_delete_all_completed_tasks -> {
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -3,11 +3,14 @@ package com.codinginflow.mvvmtodo.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.codinginflow.mvvmtodo.data.PreferencesManager
 import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 /*
     viewmodel should never reference a activity/fragment as the view model lives much longer (eg fragment destruction on orientation change)
@@ -16,29 +19,38 @@ import kotlinx.coroutines.flow.flatMapLatest
 
 // inject DAO to ViewModel
 class TasksViewModel @ViewModelInject constructor(
-    //define dependency
-    private val taskDao: TaskDao
+    //define dependencies to inject
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     // 3 mutable live data for filters
     val searchQuery = MutableStateFlow("")
     // by default sorted by date
-    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hideCompleted = MutableStateFlow(false)
+    val preferencesFlow = preferencesManager.preferencesFlow
 
     // whenever value of searchQuery/sortOrder/hideCompleted changes (emits a new value)
     // execute block (update in Dao) and assign to tasksFlow
     private val tasksFlow = combine(
         searchQuery,
-        sortOrder,
-        hideCompleted
+        preferencesFlow
     // return latest values in Triple wrapper
-    ) { query, sortOrder, hideCompleted ->
-        Triple(query, sortOrder, hideCompleted)
+    ) { query, preferences ->
+        Pair(query, preferences)
     // use latest values to execute query in DAO
-    }.flatMapLatest {(query, sortOrder, hideCompleted) ->
-        taskDao.getTasks(query, sortOrder, hideCompleted)
+    }.flatMapLatest {(query, preferences) ->
+        taskDao.getTasks(query, preferences.sortOrder, preferences.hideCompleted)
     }
-     // asLiveData is the latest value of the flow stream
+
+    // functionality for fragment to update values in preferences manager
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
+
+    // asLiveData is the latest value of the flow stream
     val tasks = tasksFlow.asLiveData()
 }
